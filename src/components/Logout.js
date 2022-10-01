2482ce4ae7141810522156550085fcd0fc0361cd
ahuiru <zhuangerlangyou@gmail.com>
2022-10-01 13:09:20 +0900

    logout

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,7 +1,23 @@
+import { signInWithPopup, signOut } from 'firebase/auth'
 import React from 'react'
+import { auth, provider } from '../firebase'
+import { useNavigate } from 'react-router-dom'
 
-export const Logout = () => {
+export const Logout = ({setIsAuth}) => {
+  const navigate = useNavigate();
+  const logout = () => {
+    //ログアウト
+    signOut(auth).then(() => {
+      localStorage.clear();
+      setIsAuth(false);
+      navigate('/login');
+    });
+  }
+
   return (
-    <div>Logout</div>
+    <div>
+      <p>ログアウトする</p>
+      <button onClick={logout}>ログアウト</button>
+    </div>
   )
 }
