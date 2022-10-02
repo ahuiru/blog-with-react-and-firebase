4c5212b28960530125043848abb6c56022a26a24
ahuiru <zhuangerlangyou@gmail.com>
2022-10-02 14:32:11 +0900

    homePage design

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,23 @@
 import React from 'react'
+import './Home.css'
 
 export const Home = () => {
+
   return (
-    <div>Home</div>
+    <div className='homePage'>
+      <div className="postContents">
+        <div className="postHeader">
+          <h1>タイトル</h1>
+        </div>
+        <div className="postTextContainer">
+          今はreactが好き。がんばっっっっっっっっっっっっっっっっっっっっっっっっっっっっっっっっっっっっっっっっっっっdろしくああああああああああああああああああああああああ
+
+        </div>
+        <div className="nameAndDeleteButton">
+          <h3>@jirou</h3>
+          <button>削除</button>
+        </div>
+      </div>
+    </div>
   )
 }
