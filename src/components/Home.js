da25c91504f70db5b4ec75a545d62279952993d6
ahuiru <zhuangerlangyou@gmail.com>
2022-10-03 18:10:08 +0900

    delete data

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import { collection, getDocs } from 'firebase/firestore'
+import { collection, deleteDoc, doc, getDocs } from 'firebase/firestore'
 import React, { useEffect, useState } from 'react'
 import { db } from '../firebase'
 import './Home.css'
@@ -18,6 +18,11 @@ export const Home = () => {
     getPosts();
   }, [])
 
+  const handleDelete = async(id) => {
+    await deleteDoc(doc(db, "posts", id));
+    window.location.href = '/';
+  }
+
   return (
     <div className='homePage'>
       {postList.map((post) => (
@@ -30,7 +35,7 @@ export const Home = () => {
           </div>
           <div className="nameAndDeleteButton">
             <h3>@{post.author.username}</h3>
-            <button>削除</button>
+            <button onClick={() => handleDelete(post.id)}>削除</button>
           </div>
         </div>
       ))}
