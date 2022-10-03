04ae5a60b651b222770daf363074895b127d0b49
ahuiru <zhuangerlangyou@gmail.com>
2022-10-03 18:19:19 +0900

    Only delete your own posts

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
 import { collection, deleteDoc, doc, getDocs } from 'firebase/firestore'
 import React, { useEffect, useState } from 'react'
-import { db } from '../firebase'
+import { auth, db } from '../firebase'
 import './Home.css'
 
 export const Home = () => {
@@ -35,7 +35,9 @@ export const Home = () => {
           </div>
           <div className="nameAndDeleteButton">
             <h3>@{post.author.username}</h3>
-            <button onClick={() => handleDelete(post.id)}>削除</button>
+            {post.author.id === auth.currentUser.uid && (
+              <button onClick={() => handleDelete(post.id)}>削除</button>
+            )}
           </div>
         </div>
       ))}
